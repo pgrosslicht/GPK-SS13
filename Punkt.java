d349ab56e3820eaac612c540e76c15ed3f6bc0c3
f1reX <firestormpro@hotmail.com>
2013-05-02 10:23:14 +0300

    Create Punkt.java

---
+++ Punkt.java
@@ -0,0 +1,10 @@
+public class Punkt {
+ 
+    public double x=0.0;
+    public double y=0.0;
+         
+    public Punkt ( double x, double y){
+        // Konstruktor
+        this.x = x;
+        this.y = y;
+    }
