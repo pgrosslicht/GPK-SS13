5b055a2ed4bf93e5a16b5b977abbdb4c335ba675
Michael Hambrusch <firestormpro@hotmail.com>
2013-05-02 23:17:52 +0200

    Kompilierungsfehler und unfertiges TestProg
    Kompilierungsfehler ist in Triangle.java, warum konnte ich nicht
    herausfinden.
    Testprog hat noch einige Punkte nicht enthalten. Habe sie ergänzt,
    konnte aber nicht kompilieren, daher auskommentiert.

--- Übungsblatt 1/Testprog.java
+++ Übungsblatt 1/Testprog.java
@@ -1,12 +1,13 @@
-/*
+﻿/*
  * TODO:
  * 
- * []
+ * [ Funktionen überprüfen! Bekomme Kompilierungsfehler bei mir ...]
  * 
  * Kontrolliert von:
- * Mike: -
+ * Mike: 130502 2305
  * Patrick: 130502 1150
  */
+
 import uebungsblatt1.*;
 
 public class Testprog {
@@ -23,5 +24,20 @@ public class Testprog {
     System.out.println("middreieck Umfang: " + middreieck.perimeter());
     System.out.println("middreieck Fläche: " + middreieck.area());
     System.out.println("x == y?: " + x.equals(y));
+
+/*
+    System.out.println("1.2   Distanz: " + x.dist(y));
+    System.out.println("1.3   Mitte: " + x.middle(y));
+    System.out.println("1.5   x == y?: " + x.equals(y));
+    System.out.println("2.2   Dreieck: " + dreieck.toString());
+    System.out.println("2.2.1 Umfang: " + dreieck.perimeter());
+    System.out.println("2.2.2 midUmfang: " + middreieck.perimeter());
+    System.out.println("2.3   midTriangle: " + middreieck.toString());
+    System.out.println("2.4.1 Flaeche: " + dreieck.area());
+    System.out.println("2.4.2 midFlaeche: " + middreieck.area());
+    System.out.println("2.6   Dreieck == middreieck?: " + dreieck.equals(middreieck)); 
+*/
+
+
   }
 }
