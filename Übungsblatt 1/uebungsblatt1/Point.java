de1138c2ec2b8fbae350d1d35bac9906103e3813
Patrick Grosslicht <pdgwien@gmail.com>
2013-05-02 11:53:43 +0200

    Point.java und Triangle.java in Package ausgelagert für leichteren Import. Point.middle() gefixed, Kompilierungsproblem von Point.java gefixed, Formatierung uniformiert.

---
+++ Übungsblatt 1/uebungsblatt1/Point.java
@@ -0,0 +1,46 @@
+/*
+ * TO DO:
+ *
+ * []
+ *
+ * Kontrolliert von:
+ * Mike: 130502 1025
+ * Patrick: 130502 1151
+ */
+package uebungsblatt1;
+
+public class Point {
+  private double x, y;
+
+  public Point(double a, double b) {
+    x = a;
+    y = b;
+  }
+
+  public double dist(Point p) {
+    double a = p.x - this.x;
+    double b = p.y - this.y;
+
+    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+  }
+
+  public Point middle(Point p) {
+    double x = 0.5 * (this.x + p.x);
+    double y = 0.5 * (this.y + p.y);
+
+    return new Point(x, y);
+  }
+
+  @Override
+  public String toString() {
+    return "(" + x + ", " + y + ")";
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    return object != null &&
+      this.getClass() == object.getClass() &&
+      this.x == ((Point) object).x &&
+      this.y == ((Point) object).y;
+  }
+}
---
+++ Übungsblatt 1/uebungsblatt1/Triangle.java
@@ -0,0 +1,48 @@
+/*
+ * TODO:
+ * 
+ * []
+ * 
+ * Kontrolliert von:
+ * Mike: 130502 1039
+ * Patrick: 130502 1151
+ */
+package uebungsblatt1;
+
+public class Triangle {
+  private Point a, b, c;
+  
+  public Triangle(Point a, Point b, Point c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  
+  public double perimeter() {
+    return this.a.dist(b) + this.b.dist(c) + this.c.dist(a);  
+  }
+  
+  public Triangle midTriangle() {
+    return new Triangle(a.middle(b), b.middle(c), c.middle(a));
+  }
+  
+  public double area() {
+    double s;
+    s = this.a.dist(b) + this.b.dist(c) + this.c.dist(a);
+    return Math.sqrt(s * (s - this.a.dist(b)) * (s - this.b.dist(c)) * (s - this.c.dist(a)));
+  }
+  
+  @Override
+  public String toString(){
+    return a.toString() + " " + b.toString() + " " +c.toString();
+  }
+  
+  @Override
+  public boolean equals(Object object) {
+    return object != null &&
+      this.getClass() == object.getClass() &&
+      a.equals(((Triangle) object).a) &&
+      b.equals(((Triangle) object).b) &&
+      c.equals(((Triangle) object).c);
+  }
+}
