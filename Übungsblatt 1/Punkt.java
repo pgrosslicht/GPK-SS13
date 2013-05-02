5948fd32b5314c73127dbb7e8165c586926a2972
f1reX <firestormpro@hotmail.com>
2013-05-02 10:37:54 +0300

    Update Punkt.java

--- Übungsblatt 1/Punkt.java
+++ Übungsblatt 1/Punkt.java
@@ -1,13 +1,1 @@
-public class Punkt {
- 
-    public double x=0.0;
-    public double y=0.0;
-         
-    
-    public Punkt ( double x, double y){
-        // Konstruktor
-        
-        this.x = x;
-        this.y = y;
- 
-    }
+LÖSCH DIE DRECKS DATEI! :D
