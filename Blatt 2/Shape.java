13f8a7b9a09b689cf0f80cde5b2cc0e8dae477f5
Patrick Grosslicht <pdgwien@gmail.com>
2013-05-07 19:53:43 +0200

    Interface Shape.java implementiert

---
+++ Blatt 2/Shape.java
@@ -0,0 +1,21 @@
+/* PK Uebung 2
+ * TODO:
+ * 
+ * []
+ * 
+ * Kontrolliert von:
+ * Mike: 
+ * Patrick: 
+ */
+
+public interface Shape {
+  /**
+   * returns perimeter of an object
+   */
+  double perimeter();
+
+  /**
+   * returns area of an object
+   */
+  double area();
+}
