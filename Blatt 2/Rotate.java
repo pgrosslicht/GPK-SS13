9366cff96fac46827a5a2e119050ec75d94c895e
Patrick Grosslicht <pdgwien@gmail.com>
2013-05-07 19:59:37 +0200

    Interface Rotate.java implementiert

---
+++ Blatt 2/Rotate.java
@@ -0,0 +1,16 @@
+/* PK Uebung 2
+ * TODO:
+ *
+ * []
+ *
+ * Kontrolliert von:
+ * Mike:
+ * Patrick:
+ */
+
+public interface Rotate {
+  /**
+   * rotiert ein Objekt um den Winkel alpha
+   */
+  void rotate(double alpha);
+}
