224e50cf0f646c9a8d761e7acaae95d0207b198e
Patrick Grosslicht <pdgwien@gmail.com>
2013-05-07 19:55:21 +0200

    Interface Scaleable.java implementiert

---
+++ Blatt 2/Scaleable.java
@@ -0,0 +1,16 @@
+/* PK Uebung 2
+ * TODO:
+ * 
+ * []
+ * 
+ * Kontrolliert von:
+ * Mike: 
+ * Patrick: 
+ */
+
+public interface Shapeable {
+  /**
+   * skaliert Objekte um sx bzw. sy
+   */
+  void scale(double sx, double xy);
+} 
