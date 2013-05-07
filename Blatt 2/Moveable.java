c25857325e5c532457ffa0d8ebe2de41b5ad36e4
Patrick Grosslicht <pdgwien@gmail.com>
2013-05-07 19:58:00 +0200

    Interface Moveable.java implementiert

---
+++ Blatt 2/Moveable.java
@@ -0,0 +1,16 @@
+/* PK Uebung 2
+ * TODO:
+ *
+ * []
+ *
+ * Kontrolliert von:
+ * Mike:
+ * Patrick:
+ */
+
+public interface Moveable {
+  /**
+   * verschiebt Objekte um den Vektor (tx, ty)
+   */
+  void move(double tx, double ty);
+}
