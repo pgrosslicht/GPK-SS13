a9f0fa6c80bf74962ca595cf86e65cd0672e328c
Patrick Grosslicht <pdgwien@gmail.com>
2013-05-09 16:01:53 +0200

    Fehler in Testprog.java gefixt, closes #1

--- Blatt 2/Testprog.java
+++ Blatt 2/Testprog.java
@@ -1,9 +1,12 @@
-/* PK Uebung 3
+/* PK Uebung 2
  * TODO:
  * 
  * [irgendwie check ich das nicht.]
  * error: Class names, 'Testprog', are only accepted if annotation processing is explicitly requested
  * 1 error
+ *
+ * benutzt du 'javac Testprog.java'? Der Fehler kommt eigentlich nur, wenn du das .java vergisst.
+ * Hab das Programm mal bisschen gefixt (du kannst dreieck.move und dergleichen nicht in println benutzen, da das void zurückgibt), bei mir  laeufts jetzt.
  *
  * Kontrolliert von:
  * Mike: 
@@ -19,20 +22,26 @@ public class Testprog {
 
     System.out.println("Dreieck das veraendert wird:");
     System.out.println("Dreieck: " + dreieck.toString()); //Dreieck Koords
-    System.out.println("Dreieck rotate: " + dreieck.rotate(5)); //rotate
+    System.out.println("Dreieck rotate");
+    dreieck.rotate(5); //rotate
     System.out.println("Dreieck: " + dreieck.toString()); //Dreieck Koords
-    System.out.println("Dreieck move: " + dreieck.move(3,2)); //move
+    System.out.println("Dreieck move"); //move
+    dreieck.move(3,2);
     System.out.println("Dreieck: " + dreieck.toString()); //Dreieck Koords
-    System.out.println("Dreieck scale: " + dreieck.rotate(5,8)); //scale
+    System.out.println("Dreieck scale"); //scale
+    dreieck.scale(5,8);
     System.out.println("Dreieck: " + dreieck.toString()); //Dreieck Koords
 
     System.out.println("Dreieck das gleich bleibt:");
     System.out.println("Dreieck: " + dreieck.toString()); //Dreieck Koords
-    System.out.println("Dreieck rotate2: " + dreieck.rotate2(5)); //rotate2
+    System.out.println("Dreieck rotate2");
+    dreieck.rotate2(5); //rotate2
     System.out.println("Dreieck: " + dreieck.toString()); //Dreieck Koords
-    System.out.println("Dreieck move2: " + dreieck.move2(3,2)); //move2
+    System.out.println("Dreieck move2");
+    dreieck.move2(3,2); //move2
     System.out.println("Dreieck: " + dreieck.toString()); //Dreieck Koords
-    System.out.println("Dreieck scale2: " + dreieck.scale2(5,8)); //scale2
+    System.out.println("Dreieck scale2");
+    dreieck.scale2(5,8); //scale2
     System.out.println("Dreieck: " + dreieck.toString()); //Dreieck Koords
 
   }
