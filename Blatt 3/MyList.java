de958a29b117c3a4e789c1a76f6e48e67b62d4ab
Michael Hambrusch <firestormpro@hotmail.com>
2013-05-19 00:32:51 +0200

    bisschen angefangen, noch nichts getestet

--- Blatt 3/MyList.java
+++ Blatt 3/MyList.java
@@ -1,17 +1,58 @@
+/* PK Uebung 3
+* TODO:
+*
+* [nicht getestet]
+*
+* Kontrolliert von:
+* Mike:
+* Patrick:
+*/
+
 public class MyList<A> {
-  private Node<A> head = null;
-
-  public boolean isEmpty() {
-    return head == null;
-  }
-
-  private class Node<A> {
-    private A val;
-    private Node<A> next = null;
-
-    Node(A v, Node<A> l) {
-      val = v;
-      next = l;
-    }
-  }
+	private Node<A> head = null;
+	
+	public boolean isEmpty() {
+		return head == null;
+	}
+	
+	private class Node<A> {
+		private A val;
+		private Node<A> next = null;
+		
+		Node(A v, Node<A> l) {
+			val = v;
+			next = l;
+		}
+	}
+	
+	/**
+	* @return gibt Anzahl der Elemente der Liste als int zurück
+	*/
+	public int size() {
+		int listCount = Mylist.size();
+		return listCount;
+	}
+	
+	/**
+	* gibt Element der Liste vom Index zurück
+	*/
+	public A get(int index) {
+		return MyList(index);
+	}
+	
+	/**
+	* fügt Element n am Listenanfang hinzu
+	*/
+	public void addFirst(A n) {
+		MyList.add(n);  
+	}	
+	
+	/**
+	* gibt das erste Element zurück und löscht es danach
+	*/
+	public A removeFirst(){
+		private double oldfirst = MyList.get(0);
+		MyList.remove(0);
+		return oldfirst;
+	}
 }
---
+++ Blatt 3/QueueI.java
@@ -0,0 +1,17 @@
+/* PK Uebung 3
+ * TODO:
+ *
+ * []
+ *
+ * Kontrolliert von:
+ * Mike:
+ * Patrick:
+ */
+
+public interface QueueI {
+  /**
+   * Erstellen Sie das Interface QueueI, das eine solche Queue reprasentiert und
+   * implementieren Sie es in MyList. Sie duerfen dabei auf alle Methoden aus der
+   * vorigen Aufgabe zugreifen.
+   */
+}
