e2e1d285df184cb6c2d6386be9b5f5b4bb650475
Michael Hambrusch <e1142232@student.tuwien.ac.at>
2013-06-10 12:37:43 +0200

    Testfälle

--- Blatt 4/Bubble.java
+++ Blatt 4/Bubble.java
@@ -1,29 +1,29 @@
 public class Bubble { 
-	public static void sortiere(int[] x) {
+	static void sort(String[] array) {
 		boolean unsortiert=true;
-		int temp;
+		String[] temp;
 		
 		while (unsortiert){
 			unsortiert = false;
-			for (int i=0; i < x.length-1; i++) 
-			if (x[i] > x[i+1]) {                      
-				temp       = x[i];
-				x[i]       = x[i+1];
-				x[i+1]     = temp;
+			for (int i=0; i < array.length-1; i++) 
+			if (array[i].compareTo(array[i+1]) > 0 ) {                      
+				temp       = array[i];
+				array[i]       = array[i+1];
+				array[i+1]     = temp;
 				unsortiert = true;
 			}          
 		} 
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("\nBest case: ");
-		int[] liste = {1,2,3,4,5,6,7,8,9};
-		sortiere(liste);
+		System.out.println("\nWorst case: ");
+		String[] liste = {"g","f","e","d","c","b","a"};
+		sort(liste);
 		for (int i=0; i<liste.length; i++) 
 			System.out.print(liste[i]+" ");  
-		System.out.println("\nWorst case: ");
-		int[] liste2 = {9,8,7,6,5,4,3,2,1};
-		sortiere(liste2);
+		System.out.println("\nBest case: ");
+		String[] liste2 = {"a","b","c","d","e","f","g"};
+		sort(liste2);
 		for (int i=0; i<liste.length; i++) 
 			System.out.print(liste[i]+" ");      
 	} 
