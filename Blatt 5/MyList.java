c820fcd17cecca7ce704c2753c0df0f6a3003151
Patrick Grosslicht <pdgwien@gmail.com>
2013-06-11 22:49:26 +0200

    Angaben hinzugefügt

---
+++ Blatt 5/MyList.java
@@ -0,0 +1,17 @@
+public class MyList<A> {
+  private class Node {
+    A value;
+    Node next;
+
+    Node(A value, Node next) {
+      this.value = value;
+      this.next = next;
+    }
+  }
+
+  private Node head = null;
+
+  public void add(A a) {
+    this.head = new Node(a, this.head);
+  }
+}
---
+++ Blatt 5/MyStringArrayIterator.java
@@ -0,0 +1,24 @@
+import java.util.Iterator;
+
+public class MyStringArrayIterator implements Iterator<String> {
+
+  // TODO Aufgabe 1.1
+
+  public MyStringArrayIterator(String[] array) {
+    // TODO Aufgabe 1.1
+  }
+
+  public boolean hasNext() {
+    // TODO Aufgabe 1.1
+    return false;
+  }
+
+  public String next() {
+    // TODO Aufgabe 1.1
+    return "";
+  }
+
+  public void remove() {
+    // TODO Aufgabe 1.2
+  }
+}
