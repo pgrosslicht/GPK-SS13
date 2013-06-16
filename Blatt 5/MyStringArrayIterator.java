5f43082b17937d7aa46a3b18f3f4ef25d69b62f6
Michael Hambrusch <firestormpro@hotmail.com>
2013-06-16 19:36:58 +0200

    1.2 remove()

--- Blatt 5/MyStringArrayIterator.java
+++ Blatt 5/MyStringArrayIterator.java
@@ -1,22 +1,32 @@
 import java.util.Iterator;
 
 public class MyStringArrayIterator implements Iterator<String> {
-  private String[] theArray;
-  private int index = 0;
-
-  public MyStringArrayIterator(String[] array) {
-    this.theArray = array;
-  }
-
-  public boolean hasNext() {
-    return index < this.theArray.length;
-  }
-
-  public String next() {
-    return theArray[index++];
-  }
-
-  public void remove() {
-    // TODO Aufgabe 1.2
-  }
+	
+	private String[] theArray;
+	private int index = 0;
+	
+	public MyStringArrayIterator(String[] array) {
+		this.theArray = array;
+	}
+	
+	public boolean hasNext() {
+		return index < this.theArray.length;
+	}
+	
+	public String next() {
+		return theArray[index++];
+	}
+	
+	public void remove() {
+		String[] tempArray = new String[this.theArray.length-1];
+		for(int i = 0; i < tempArray.length; i++) {
+			if(i < index) {
+				tempArray[i] = this.theArray[i];
+			}
+			else { // i >= index
+				tempArray[i] = this.theArray[i+1];
+			}
+			this.theArray = tempArray;
+		}
+	}
 }
---
+++ Blatt 5/test.java
@@ -0,0 +1,9 @@
+
+
+public class test {
+	
+	public static void main(String[] args){
+			int i = 0;
+			System.out.println("Testprogramm i + 5: " + (i+5));		
+	}
+}
