e3ae91e2fcfd7a2edad7fc7b81cb0a1b0e0b2d3d
Michael Hambrusch <firestormpro@hotmail.com>
2013-06-16 21:49:51 +0200

    Testprog fixed()

--- Blatt 5/MyList.java
+++ Blatt 5/MyList.java
@@ -37,23 +37,23 @@ public class MyList<A> implements Iterable<A> {
         }
         
         public static void main(String[] args) {
-    MyList<Integer> test = new MyList<Integer>(); // neue Liste
-    test.add(0); //fügt 0 hinzu
-    test.add(1); //fügt 1 hinzu
-    test.add(2); //fügt 2 hinzu
-    test.add(3); //fügt 3 hinzu
-    test.add(4); //fügt 4 hinzu
-    test.add(5); //fügt 5 hinzu
-    test.add(6); //fügt 6 hinzu
-    MyList.MyListIterator test2 = test.iterator();
-    System.out.println(test.head.value); //sollte null ausgeben
-    System.out.println(test2.hasNext()); //sollte true ausgeben
-    System.out.println(test2.next()); //sollte 0 ausgeben
-    System.out.println(test2.next()); //sollte 1 ausgeben
-    System.out.println(test.head.value); //sollte 1 ausgeben
-    test2.remove(); //sollte 1 removen
-    System.out.println(test.head.value); //sollte null ausgeben (weil 1 removed wurde)
-    System.out.println(test2.next()); //sollte 2 ausgeben
-    System.out.println(test.head.value); //sollte 2 ausgeben
+                MyList<Integer> test = new MyList<Integer>(); // neue Liste
+                test.add(2); //add 2
+                test.add(3); //add 3
+                test.add(4); //add 4
+                test.add(5); //add 5
+                test.add(6); //add 6
+                MyList.MyListIterator test2 = test.iterator();
+                System.out.println(test.head.value); //sollte 6 ausgeben
+                System.out.println(test2.hasNext()); //sollte true (5) ausgeben
+                System.out.println(test2.next()); //sollte 5 ausgeben
+                System.out.println(test2.next()); //sollte 4 ausgeben
+                System.out.println(test.head.value); //sollte 4 ausgeben
+                test2.remove(); //sollte 4 removen
+                System.out.println(test.head.value); //sollte 3 ausgeben (weil 4 removed und head jetzt auf head.next zeigt)
+                System.out.println(test2.next()); //sollte 2 ausgeben
+                System.out.println(test.head.value); //sollte 2 ausgeben
+                System.out.println(test2.hasNext()); //sollte false ausgeben (weil kein weiteres Element)
+                System.out.println(test2.next()); //sollte Exception werfen
         }
 }
