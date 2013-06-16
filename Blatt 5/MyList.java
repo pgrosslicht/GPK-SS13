a739ba809ebd442c4428f6deb19399ee5b754a18
Patrick Grosslicht <pdgwien@gmail.com>
2013-06-16 21:39:48 +0200

    MyListIterator() entfernt

--- Blatt 5/MyList.java
+++ Blatt 5/MyList.java
@@ -16,10 +16,6 @@ public class MyList<A> implements Iterable<A> {
         }
         
         private class MyListIterator implements Iterator<A> {
-                public MyListIterator() {
-                        
-                }
-                
                 public void remove() {
                         head = head.next;
                 }
@@ -41,23 +37,23 @@ public class MyList<A> implements Iterable<A> {
         }
         
         public static void main(String[] args) {
-		MyList<Integer> test = new MyList<Integer>(); // neue Liste
-		test.add(0); //fügt 0 hinzu
-		test.add(1); //fügt 1 hinzu
-		test.add(2); //fügt 2 hinzu
-		test.add(3); //fügt 3 hinzu
-		test.add(4); //fügt 4 hinzu
-		test.add(5); //fügt 5 hinzu
-		test.add(6); //fügt 6 hinzu
+    MyList<Integer> test = new MyList<Integer>(); // neue Liste
+    test.add(0); //fÃ¼gt 0 hinzu
+    test.add(1); //fÃ¼gt 1 hinzu
+    test.add(2); //fÃ¼gt 2 hinzu
+    test.add(3); //fÃ¼gt 3 hinzu
+    test.add(4); //fÃ¼gt 4 hinzu
+    test.add(5); //fÃ¼gt 5 hinzu
+    test.add(6); //fÃ¼gt 6 hinzu
     MyList.MyListIterator test2 = test.iterator();
-		System.out.println(test.head.value); //sollte null ausgeben
-		System.out.println(test2.hasNext()); //sollte true ausgeben
-		System.out.println(test2.next()); //sollte 0 ausgeben
-		System.out.println(test2.next()); //sollte 1 ausgeben
-		System.out.println(test.head.value); //sollte 1 ausgeben
-		test2.remove(); //sollte 1 removen
-		System.out.println(test.head.value); //sollte null ausgeben (weil 1 removed wurde)
-		System.out.println(test2.next()); //sollte 2 ausgeben
-		System.out.println(test.head.value); //sollte 2 ausgeben
+    System.out.println(test.head.value); //sollte null ausgeben
+    System.out.println(test2.hasNext()); //sollte true ausgeben
+    System.out.println(test2.next()); //sollte 0 ausgeben
+    System.out.println(test2.next()); //sollte 1 ausgeben
+    System.out.println(test.head.value); //sollte 1 ausgeben
+    test2.remove(); //sollte 1 removen
+    System.out.println(test.head.value); //sollte null ausgeben (weil 1 removed wurde)
+    System.out.println(test2.next()); //sollte 2 ausgeben
+    System.out.println(test.head.value); //sollte 2 ausgeben
         }
 }
