9f153ff71f4b32b28f8976a471054ea569fea2cc
Michael Hambrusch <firestormpro@hotmail.com>
2013-06-23 18:42:26 +0200

    preorder traversal

--- Blatt 6/MyTreeSet.java
+++ Blatt 6/MyTreeSet.java
@@ -1,7 +1,7 @@
 public class MyTreeSet<A extends Comparable<A>> {
     private TreeNode root = null;
     
-    class TreeNode { // Knoten im Baum
+    public class TreeNode { // Knoten im Baum
         private A elem;  //Element
         private TreeNode left = null;   // linker Teilbaum
         private TreeNode right = null;  // rechter Teilbaum
@@ -12,72 +12,81 @@ public class MyTreeSet<A extends Comparable<A>> {
             this.left = null;
             this.right = null;
         }
-    
-    public void add(A a) {
-      if (a.compareTo(this.elem) < 0) {
-        if (this.left != null) {
-          this.left.add(a);
-        } else {
-          this.left = new TreeNode(a);
-      System.out.println("new node left" + this.elem);
+        
+        public TreeNode getLeft()
+        {
+            return this.left;
         }
-      } else {
-        if (this.right != null) {
-          this.right.add(a);
-        } else {
-          this.right = new TreeNode(a);
-      System.out.println("new node right" + this.elem);
+        public TreeNode getRight()
+        {
+            return this.right;
         }
-      }
-    }
-   
+        
+        public void add(A a) {
+            if (a.compareTo(this.elem) < 0) {
+                if (this.left != null) {
+                    this.left.add(a);
+                } else {
+                    this.left = new TreeNode(a);
+                    System.out.println("new node left " + this.elem);
+                }
+            } else {
+                if (this.right != null) {
+                    this.right.add(a);
+                } else {
+                    this.right = new TreeNode(a);
+                    System.out.println("new node right " + this.elem);
+                }
+            }
+        }
+        
         /*
         //2.1 boolean add(A a)
         //Laufzeit: ???
         boolean add(A a) {
-            boolean hinzu = false;
-            if( a < elem ) {
-                if ( left != null ) {
-                    left.add(a);
-                }
-                else {
-                    left = new TreeNode(a);////////
-                    hinzu = true;
-                }
-            }
-            else if( a > elem ) {
-                if ( right != null ) {
-                    right.add(e);
-                }
-                else {
-                    right = new TreeNode(a); //////////
-                    hinzu = true;
-                }
-            }
-            
-            return hinzu; // a nicht größer oder kleiner als elem --> a == elem
+        boolean hinzu = false;
+        if( a < elem ) {
+        if ( left != null ) {
+        left.add(a);
+        }
+        else {
+        left = new TreeNode(a);////////
+        hinzu = true;
+        }
+        }
+        else if( a > elem ) {
+        if ( right != null ) {
+        right.add(e);
+        }
+        else {
+        right = new TreeNode(a); //////////
+        hinzu = true;
+        }
+        }
+        
+        return hinzu; // a nicht größer oder kleiner als elem --> a == elem
         }
         
         //2.2 booblean remove(A a)
         //Laufzeit: 
         boolean remove(A a) {
-            boolean del = false;
-            if( a < elem ) {
-                if( left != null ) {
-                    left = left.remove(a);
-                }
-            }
-            else if( a == elem ) {
-                if( right == null ) {
-                    del = true;
-                } 
-            }
-            else if( a > elem ) {
-                if( right != null ) {
-                    right = right.remove(a);   
-                }
-            }
-            return del;        
+        boolean del = false;
+        if( a < elem ) {
+        if( left != null ) {
+        left = left.remove(a);
+        }
+        }
+        else if( a == elem ) {
+        if( right == null ) {
+        del = true;
+        } 
+        }
+        else if( a > elem ) {
+        if( right != null ) {
+        right = right.remove(a);   
+        }
+        }
+        return del;        
         }
         */
         //2.3 boolean contains(A a)
@@ -100,13 +109,13 @@ public class MyTreeSet<A extends Comparable<A>> {
             int size=0;
             return size;        
         }
+        
+    }
     
-     }
-     
-     
-  public void MyTreeSet() {
-    root = null;
-  }
+    
+    public void MyTreeSet() {
+        root = null;
+    }
     
     //Hinzufügen von Elementen (Zahlen) zum Testen
     public void add(A a) {
@@ -126,6 +135,17 @@ public class MyTreeSet<A extends Comparable<A>> {
         return "";
     }
     
+    
+    public static void preorder(TreeNode n)
+    {
+        if (n != null)
+        {
+            System.out.print(n.elem + " ");
+            preorder(n.getLeft());
+            preorder(n.getRight());
+        }
+    }
+    
     //Testprogramm
     public static void main(String[] args) {
         MyTreeSet<Integer> testbaum = new MyTreeSet<Integer>();
@@ -136,8 +156,19 @@ public class MyTreeSet<A extends Comparable<A>> {
         testbaum.add(2);
         testbaum.add(4);
         testbaum.add(7);
-        System.out.println(testbaum.toString());
+        preorder(testnode); //soll ausgeben: 543267
         
+        
+        TreeNode<Integer> testnode = new TreeNode<Integer>();
+        testnode.add(5);
+        testnode.add(4);
+        testnode.add(6);
+        testnode.add(3);
+        testnode.add(2);
+        testnode.add(4);
+        testnode.add(7);
+        //    System.out.println(testbaum.toString());
+        preorder(testnode); //soll ausgeben: 543267
         
     }
 }
