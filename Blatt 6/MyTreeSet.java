9a2a5620ad7fa8b928e4022707767efcedf28a58
Michael Hambrusch <firestormpro@hotmail.com>
2013-06-23 13:39:59 +0200

    2.1 add(A a)

--- Blatt 6/MyTreeSet.java
+++ Blatt 6/MyTreeSet.java
@@ -1,42 +1,79 @@
-
-class TreeNode<A extends Comparable<A>> { // Knoten im Baum
-    private A elem;  //Element
-    private TreeNode<A> left = null;   // linker Teilbaum
-    private TreeNode<A> right = null;  // rechter Teilbaum
+public class MyTreeSet {
+    private TreeNode root = null;
     
-    //Konstruktor für elem
-    TreeNode (A a) {
-        elem = a;
-    }
-    
-    //2.1 boolean add(A a)
-    boolean add(A a) {
-        return true;
-    }
-    
-    //2.2 booblean remove(A a)
-    boolean remove(A a) {
-        return true;        
-    }
-    
-    //2.3 boolean contains(A a)
-    boolean contains(A a) {
-        int c = a.compareTo(elem);
-        if( c < 0 ) {
-            return left != null && left.contains(a);
+    class TreeNode<A extends Comparable<A>> { // Knoten im Baum
+        private A elem;  //Element
+        private TreeNode<A> left = null;   // linker Teilbaum
+        private TreeNode<A> right = null;  // rechter Teilbaum
+        
+        //Konstruktor für elem
+        TreeNode (A a) {
+            elem = a;
         }
-        else if( c == 0) {
-            return true;   
+        
+        //2.1 boolean add(A a)
+        //Laufzeit: ???
+        boolean add(A a) {
+            boolean hinzu = false;
+            if( a < elem ) {
+                if ( left != null ) {
+                    left.add(a);
+                }
+                else {
+                    left = new TreeNode(a);////////
+                    hinzu = true;
+                }
+            }
+            else {
+                if ( right != null ) {
+                    right.add(e);
+                }
+                else {
+                    right = new TreeNode(a); //////////
+                    hinzu = true;
+                }
+            }
+            
+            return hinzu;
+        }
+        
+        //2.2 booblean remove(A a)
+        //Laufzeit: 
+        boolean remove(A a) {
+            return true;        
+        }
+        
+        //2.3 boolean contains(A a)
+        //Laufzeit: linear
+        boolean contains(A a) {
+            int c = a.compareTo(elem);
+            if( c < 0 ) {
+                return left != null && left.contains(a);
+            }
+            else if( c == 0) {
+                return true;   
+            }
+            else {
+                return right != null && right.contains(a);   
+            }
+        }
+        
+        //2.4 int size()
+        int size() {
+            return true;        
+        }
+        
+    }
+    
+    
+    //Hinzufügen von Elementen (Zahlen) zum Testen
+    public void add(int a) {
+        if( root == null ) {
+            root = new TreeNode(a);
         }
         else {
-            return right != null && right.contains(a);   
+            root.add(a);
         }
-    }
-    
-    //2.4 int size()
-    int size() {
-        return true;        
     }
     
 }
-
