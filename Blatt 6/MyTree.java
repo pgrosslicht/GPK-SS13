98d9ac635d4690546b309d81517fe70da4bd908a
Patrick Grosslicht <pdgwien@gmail.com>
2013-06-23 12:08:28 +0200

    Skelett weiter ausgebaut

--- Blatt 6/MyTree.java
+++ Blatt 6/MyTree.java
@@ -1,9 +1,28 @@
 public class MyTree<A extends Comparable<A>> {
-  private Node<A> root = null;
+  private Node<A> root;
   private class Node<A> {
+    private A element;
+    private Node<A> left_child;
+    private Node<A> right_child;
 
+    public void Node(A e) {
+      element = e;
+    }
+
+    public void add(A e) {
+    
+    }
   }
-  public MyTree<A> MyTree() {
-    return null;
+  public void MyTree() {
+    root = null;
+  }
+
+  public void add(A elem) {
+    if (root == null) {
+      root = new Node(elem);
+    } else {
+      root.add(elem);
+    }
   }
 }
+
