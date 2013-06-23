05a58aa8416587053180bff878cf70d6aab65ae8
Patrick Grosslicht <pdgwien@gmail.com>
2013-06-23 13:37:24 +0200

    Grundlegende Baumfunktion in MyTree.java

--- Blatt 6/MyTree.java
+++ Blatt 6/MyTree.java
@@ -1,18 +1,35 @@
 public class MyTree<A extends Comparable<A>> {
-  private Node<A> root;
-  private class Node<A> {
+  private Node root;
+  private class Node {
     private A element;
-    private Node<A> left_child;
-    private Node<A> right_child;
+    private Node left_child;
+    private Node right_child;
 
-    public void Node(A e) {
-      element = e;
+    Node(A e) {
+      this.element = e;
+      this.left_child = null;
+      this.right_child = null;
     }
 
     public void add(A e) {
-    
+      if (e.compareTo(this.element) < 0) {
+        if (this.left_child != null) {
+          this.left_child.add(e);
+        } else {
+          this.left_child = new Node(e);
+      System.out.println("new node left" + this.element);
+        }
+      } else {
+        if (this.right_child != null) {
+          this.right_child.add(e);
+        } else {
+          this.right_child = new Node(e);
+      System.out.println("new node right" + this.element);
+        }
+      }
     }
   }
+
   public void MyTree() {
     root = null;
   }
@@ -20,9 +37,28 @@ public class MyTree<A extends Comparable<A>> {
   public void add(A elem) {
     if (root == null) {
       root = new Node(elem);
+      System.out.println("test");
     } else {
       root.add(elem);
     }
   }
-}
 
+  public String toString() {
+    if (root != null) {
+      return root.toString();
+    }
+    return "";
+  }
+
+  public static void main(String[] args) {
+    MyTree<Integer> mytree = new MyTree<Integer>();
+    mytree.add(5);
+    mytree.add(4);
+    mytree.add(6);
+    mytree.add(3);
+    mytree.add(2);
+    mytree.add(4);
+    mytree.add(7);
+    System.out.println(mytree.toString());
+  }
+}
