48d11245a3f3caff34f9a659fcc001254a82760a
Patrick Grosslicht <pdgwien@gmail.com>
2013-06-23 11:50:04 +0200

    innere Nodeklasse + Konstruktor erstellt

--- Blatt 6/MyTree.java
+++ Blatt 6/MyTree.java
@@ -1,3 +1,9 @@
-public class MyTree {
+public class MyTree<A extends Comparable<A>> {
+  private Node<A> root = null;
+  private class Node<A> {
 
+  }
+  public MyTree<A> MyTree() {
+    return null;
+  }
 }
